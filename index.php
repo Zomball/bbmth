b177924a8bf2254b3130e0c73df09a3d6a1e97bb
Zomball <162007513+Zomball@users.noreply.github.com>
2024-03-03 14:41:07 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-874" />
+<title>BBMThailand Club</title>
+<style type="text/css">
+<!--
+body {
+	background-color: #dfd8d0;
+}
+#newsbox {
+	height: 125px;
+	overflow: auto;
+}
+a:link {
+	color: #0000EE;
+}
+a:visited {
+	color: #0000EE;
+}
+a:hover {
+	color: #0000EE;
+}
+-->
+</style>
+<script type="text/javascript">
+<!--
+function MM_popupMsg(msg) { //v1.0
+  alert(msg);
+}
+//-->
+var popUpWin=0;
+function popUpWindow(URLStr, left, top, width, height)
+{
+  if(popUpWin)
+  {
+    if(!popUpWin.closed) popUpWin.close();
+  }
+  popUpWin = open(URLStr, 'popUpWin', 'toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=no,resizable=no,copyhistory=yes,width='+width+',height='+height+',left='+left+', top='+top+',screenX='+left+',screenY='+top+'');
+}
+</script>
+</head>
+
+<body>
+<table width="800" height="550" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td width="800" height="550" valign="top" background="index.jpg"><table width="800" height="200" border="0" cellpadding="0" cellspacing="0">
+      <tr>
+        <td>&nbsp;</td>
+      </tr>
+    </table>
+      <table width="800" height="170" border="0" cellpadding="0" cellspacing="0">
+        <tr>
+          <td align="center"><table width="570" height="125" border="0" cellpadding="0" cellspacing="0">
+            <tr>
+              <td align="left" valign="top"><div id="newsbox"><?php require_once('news.html'); ?></div></td>
+            </tr>
+          </table></td>
+        </tr>
+      </table>
+      <table width="800" height="100" border="0" cellpadding="0" cellspacing="0">
+        <tr>
+          <td width="50">&nbsp;</td>
+          <td width="0" align="center" valign="middle"><!--<a href="https://www.facebook.com/groups/bbmth/"><img src="img/index/b01.png" width="200" height="60" border="0" />--></a></td>
+<td width="0" align="center" valign="middle"><!--<a href="regis.html"><img src="img/index/b02.png" width="200" height="60" border="0" />--></a></td>
+          <td width="50">&nbsp;</td>
+        </tr>
+      </table>
+      <table width="800" height="80" border="0" cellpadding="0" cellspacing="0">
+        <tr>
+          <td>&nbsp;</td>
+        </tr>
+          <td width="300" align="center" valign="middle"><!--<a href="https://www.facebook.com/groups/bbmth/"><img src="img/index/b01.png" width="200" height="60" border="0" />--></a></td>
+          <td width="300" align="center" valign="middle"><!--<a href="https://www.facebook.com/groups/bbmth/"><img src="img/index/b01.png" width="200" height="60" border="0" />--></a></td>
+      </table></td>
+  </tr>
+</table>
+</body>
+</html>
